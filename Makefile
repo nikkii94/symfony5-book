e516761ee85d1d88f2699de7ab22842c2e6b4682
nikkii94 <k.nikolett17@gmail.com>
2020-02-15 21:13:12 +0100

    Symfony 5 book base install with docker

---
+++ Makefile
@@ -0,0 +1,25 @@
+app_container_name=sf5_app
+nginx_container_name=sf5_nginx
+
+build:
+	@docker-compose -f docker-compose.yml build
+
+start:
+	@docker-compose -f docker-compose.yml up -d && start https://symfony5book.local
+
+stop:
+	@docker-compose down
+
+config:
+	@docker-compose -f docker-compose.yml config
+
+ssh:
+	@docker exec -it $(app_container_name) bash
+
+export-ssl:
+	@docker cp $(nginx_container_name):/etc/ssl ./certs
+
+#check: composer-validate cs-check phpstan psalm
+#
+#composer-validate: ensure composer-normalize-check
+#	sh -c "${PHPQA_DOCKER_COMMAND} composer validate"
---
+++ app/src/Migrations/Version20200215200512.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200215200512 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE comment_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE conference_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE comment (id INT NOT NULL, author VARCHAR(255) NOT NULL, text TEXT NOT NULL, email VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE conference (id INT NOT NULL, city VARCHAR(255) NOT NULL, year VARCHAR(4) NOT NULL, is_international BOOLEAN NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE comment_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE conference_id_seq CASCADE');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE conference');
+    }
+}
