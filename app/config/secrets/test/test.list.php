1e1e97542482ce1f84f045040835bbda42690658
nikkii94 <k.nikolett17@gmail.com>
2020-02-18 21:53:25 +0100

    Basic functional test

---
+++ app/config/secrets/test/test.list.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+  'AKISMET_KEY' => NULL,
+  'DATABASE_URL' => NULL,
+);
--- app/tests/SpamCheckerTest.php
+++ app/tests/SpamCheckerTest.php
@@ -5,11 +5,23 @@ namespace App\Tests;
 use App\Entity\Comment;
 use App\SpamChecker;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class SpamCheckerTest extends TestCase
 {
+    /**
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
     public function testSpamScoreWithInvalidRequest() :void
     {
         $comment = new Comment();
@@ -25,8 +37,45 @@ class SpamCheckerTest extends TestCase
         )]);
         $checker = new SpamChecker($client, 'abcde');
 
-        $this->expectException(\RuntimeException::class);
+        $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage('Unable to check for spam: invalid (Invalid key).');
         $checker->getSpamScore($comment, $context);
+    }
+
+    /**
+     * @dataProvider getComments
+     * @param int $expectedScore
+     * @param ResponseInterface $response
+     * @param Comment $comment
+     * @param array $context
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function testSpamScore(int $expectedScore, ResponseInterface $response, Comment $comment, array $context) :void
+    {
+        $client = new MockHttpClient([$response]);
+        $checker = new SpamChecker($client, 'abcde');
+
+        $score = $checker->getSpamScore($comment, $context);
+        $this->assertSame($expectedScore, $score);
+    }
+
+    public function getComments() :iterable {
+
+        $comment = new Comment();
+        $comment->setCreatedAtValue();
+
+        $context = [];
+
+        $response = new MockResponse('', ['response_headers' => ['x-akismet-pro-tip: discard']]);
+        yield 'blatant_spam' => [2, $response, $comment, $context];
+
+        $response = new MockResponse('true');
+        yield 'spam' => [1, $response, $comment, $context];
+
+        $response = new MockResponse('false');
+        yield 'ham' => [0, $response, $comment, $context];
     }
 }
